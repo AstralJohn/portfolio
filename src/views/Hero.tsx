2ec7d7ab8f40aae57c7002923954337358d70d1c
John Dinh <johnduydinh@gmail.com>
2021-01-01 19:18:52 -0500

    improve styling for tablet view

--- src/views/Hero.tsx
+++ src/views/Hero.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import Button from 'components/Button'
 import headshot150 from 'assets/me-150w.png'
-import headshot300 from 'assets/me-300w.png'
+import headshotStyled from 'assets/me-styled-300w.png'
 
 const Hero: React.FC = () => {
   return (
-    <header className="px-10 bg-blue-dark md:bg-white text-blue-light md:text-blue-dark text-center md:text-left md:grid gap-8 grid-cols-3 py-16">
-      <picture className="col-span-1 order-1 md:flex items-center">
-        <source srcSet={headshot300} media="(min-width: 768px)" />
+    <header className="px-10 bg-blue-dark md:bg-white text-blue-light md:text-blue-dark text-center md:text-left md:grid grid-cols-12 py-16">
+      <picture className="col-span-4 md-2:col-span-5 order-1 md:flex items-end">
+        <source srcSet={headshotStyled} media="(min-width: 768px)" />
         <img
-          className="mx-auto mb-5 rounded-full md:rounded-none bg-blue-light md:bg-white border-blue-light border-4 md:border-none"
+          className="mx-auto md:mr-0 mb-5 md:mb-0 rounded-full md:rounded-none bg-blue-light md:bg-white border-blue-light border-4 md:border-none"
           src={headshot150}
           alt="A headshot photo of John Dinh"
         />
       </picture>
-      <div className="col-span-2">
+      <div className="col-span-8 md-2:col-span-7">
         <h1 className="text-5xl mb-1">Hi, I&apos;m John</h1>
 
         <h2 className="text-lg tracking-tight mb-8">
@@ -31,12 +31,14 @@ const Hero: React.FC = () => {
 
         <div className="block xs:flex md:block justify-around text-white">
           <Button
-            className="mb-8 xs:mb-0 block md:inline-block md:mr-10 mx-auto xs:mx-0"
+            className="mb-8 xs:mb-0 block md:inline-block mx-auto xs:mx-0 md:mr-5 md-2:mr-16"
             bgColor="bg-blue"
           >
             View Work
           </Button>
-          <Button bgColor="bg-orange">Contact</Button>
+          <Button style={{ transform: 'translateY(-1px)' }} bgColor="bg-orange">
+            Contact
+          </Button>
         </div>
       </div>
     </header>
