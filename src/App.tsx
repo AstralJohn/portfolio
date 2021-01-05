d6ca2fb721ebcacd4d1305404ee7172a12f170f5
John Dinh <johnduydinh@gmail.com>
2021-01-05 06:29:05 -0500

    initial commit for my work section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero from 'views/Hero'
+import MyWork from 'views/MyWork'
 const App: React.FC = () => {
   return (
     <>
       <Hero />
+      <MyWork />
     </>
   )
 }
