048b5733c310d00ae655b2f7799d7f3e8309093d
John Dinh <johnduydinh@gmail.com>
2021-01-06 06:42:45 -0500

    create initial browser modal for projects

--- src/views/MyWork/index.tsx
+++ src/views/MyWork/index.tsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import BrowserModal from './browserModal'
+import pocketnow480 from 'assets/pocketnow-480w.jpg'
+import pocketnow960 from 'assets/pocketnow-960w.jpg'
+import Button from 'components/Button'
 
 const MyWork: React.FC = () => {
-  return <div>Hello World</div>
+  return (
+    <section className="text-center container mx-auto">
+      <h1 className="text-6xl mb-24">My Work</h1>
+      <BrowserModal
+        data={{
+          pictureData: [{ src: pocketnow960 }],
+          title: 'PocketNow',
+          src: pocketnow480,
+          alt: 'Screenshot of PocketNow.com'
+        }}
+      />
+      <BrowserModal
+        data={{
+          pictureData: [{ src: pocketnow960 }],
+          title: 'PocketNow',
+          src: pocketnow480,
+          alt: 'Screenshot of PocketNow.com'
+        }}
+      />
+      <BrowserModal
+        data={{
+          pictureData: [{ src: pocketnow960 }],
+          title: 'PocketNow',
+          src: pocketnow480,
+          alt: 'Screenshot of PocketNow.com'
+        }}
+      />
+      <Button className="text-white shadow-lg" bgColor="bg-blue">View More</Button>
+    </section>
+  )
 }
 
 export default MyWork
