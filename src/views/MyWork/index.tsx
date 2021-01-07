23b7d87f58a0aeaedd8ad6a339da3bf13496e497
John Dinh <johnduydinh@gmail.com>
2021-01-07 09:02:35 -0500

    complete initial My Work section

--- src/views/MyWork/index.tsx
+++ src/views/MyWork/index.tsx
@@ -1,38 +1,84 @@
 import React from 'react'
-import BrowserModal from './browserModal'
+import Project from './project'
 import pocketnow480 from 'assets/pocketnow-480w.jpg'
 import pocketnow960 from 'assets/pocketnow-960w.jpg'
-import Button from 'components/Button'
 
 const MyWork: React.FC = () => {
   return (
     <section className="text-center container mx-auto">
-      <h1 className="text-6xl mb-24">My Work</h1>
-      <BrowserModal
-        data={{
-          pictureData: [{ src: pocketnow960 }],
-          title: 'PocketNow',
-          src: pocketnow480,
-          alt: 'Screenshot of PocketNow.com'
-        }}
-      />
-      <BrowserModal
-        data={{
-          pictureData: [{ src: pocketnow960 }],
-          title: 'PocketNow',
-          src: pocketnow480,
-          alt: 'Screenshot of PocketNow.com'
-        }}
-      />
-      <BrowserModal
-        data={{
-          pictureData: [{ src: pocketnow960 }],
-          title: 'PocketNow',
-          src: pocketnow480,
-          alt: 'Screenshot of PocketNow.com'
-        }}
-      />
-      <Button className="text-white shadow-lg" bgColor="bg-blue">View More</Button>
+      <h1 className="text-6xl mb-3">My Work</h1>
+      <p className="px-5 mb-14">
+        Here are a few projects I have had the pleasure to worked on recently.
+        Want more details on my projects?{' '}
+        <div>
+          <a className="font-bold text-blue" href="mailto:john@johndinh.dev">
+            Email me.
+          </a>
+        </div>
+      </p>
+
+      <div>
+        <Project
+          data={{
+            pictureData: [{ src: pocketnow960 }],
+            title: 'PocketNow',
+            desc:
+              'High traffic, WordPress news site about phones and electronics.',
+            src: pocketnow480,
+            alt: 'Screenshot of PocketNow.com'
+          }}
+        />
+        <Project
+          data={{
+            pictureData: [{ src: pocketnow960 }],
+            title: 'PocketNow',
+            desc:
+              'High traffic, WordPress news site about phones and electronics.',
+            src: pocketnow480,
+            alt: 'Screenshot of PocketNow.com'
+          }}
+        />
+        <Project
+          data={{
+            pictureData: [{ src: pocketnow960 }],
+            title: 'PocketNow',
+            desc:
+              'High traffic, WordPress news site about phones and electronics.',
+            src: pocketnow480,
+            alt: 'Screenshot of PocketNow.com'
+          }}
+        />
+        <Project
+          data={{
+            pictureData: [{ src: pocketnow960 }],
+            title: 'PocketNow',
+            desc:
+              'High traffic, WordPress news site about phones and electronics.',
+            src: pocketnow480,
+            alt: 'Screenshot of PocketNow.com'
+          }}
+        />
+        <Project
+          data={{
+            pictureData: [{ src: pocketnow960 }],
+            title: 'PocketNow',
+            desc:
+              'High traffic, WordPress news site about phones and electronics.',
+            src: pocketnow480,
+            alt: 'Screenshot of PocketNow.com'
+          }}
+        />
+        <Project
+          data={{
+            pictureData: [{ src: pocketnow960 }],
+            title: 'PocketNow',
+            desc:
+              'High traffic, WordPress news site about phones and electronics.',
+            src: pocketnow480,
+            alt: 'Screenshot of PocketNow.com'
+          }}
+        />
+      </div>
     </section>
   )
 }
