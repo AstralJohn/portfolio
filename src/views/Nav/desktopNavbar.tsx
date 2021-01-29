d6a0594e391f4f7537f9f9ea4048b41815cf1f66
John Dinh <johnduydinh@gmail.com>
2021-01-29 15:00:52 -0500

    wip scroll down and scroll up logic for nav

--- src/views/Nav/desktopNavbar.tsx
+++ src/views/Nav/desktopNavbar.tsx
@@ -1,29 +1,65 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { Link, animateScroll as scroll } from 'react-scroll'
 
 const DesktopNavbar: React.FC = () => {
   const linkStyles =
-    'cursor-pointer font-headings font-medium pr-24 text-xl 2xl:text-2xl'
-  const handleScroll = () => {}
+    'z-50 cursor-pointer font-headings font-medium pl-24 text-xl 2xl:text-2xl'
 
+  let scrollYPosition = 0
+
+  const initialNavStyles = {
+    nav: 'relative'
+  }
+
+  const navRef = useRef(null)
+
+  const [navStyles, setNavStyles] = useState(initialNavStyles)
+
+  const handleScrollDirection = () => {
+    const currentYPosition = window.pageYOffset
+    if (currentYPosition > scrollYPosition || currentYPosition === 0) {
+      // down scroll
+      document.getElementById('root')!.style.paddingTop = '0px'
+
+      setNavStyles({
+        nav: 'relative'
+      })
+    } else {
+      // @ts-ignore
+      const navHeight = navRef.current.offsetHeight
+      // up scroll
+      setNavStyles({
+        nav: 'fixed bg-blue-dark right-0 left-0 text-white'
+      })
+
+      document.getElementById('root')!.style.paddingTop = navHeight + 'px'
+    }
+
+    scrollYPosition = currentYPosition <= 0 ? 0 : currentYPosition
+  }
+
+  // Initialize event listener for scrolling
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener('scroll', handleScrollDirection)
     return () => {
-      window.removeEventListener('scroll', handleScroll)
+      window.removeEventListener('scroll', handleScrollDirection)
     }
   }, [])
 
   return (
-    <nav className="hidden lg:block fixed top-0 bg-blue-dark right-0 left-0 text-white z-50">
-      <div className="2xl:pl-32 px-10 4k:pl-60">
+    <nav
+      ref={navRef}
+      className={'hidden mb-4 lg:block z-50 transition-all top-0' + ' ' + navStyles.nav}
+    >
+      <div className="2xl:px-32 px-10 4k:px-60 flex justify-between">
         <Link
           onClick={() => scroll.scrollToTop()}
           to="root"
-          className="cursor-pointer font-display text-xl text-white bg-blue-dark px-5 lg:pl-2 lg:py-4 2xl:py-7 mr-24 inline-block"
+          className="cursor-pointer font-display text-xl text-white bg-blue-dark px-5 lg:py-4 2xl:py-7 mr-24 inline-block"
         >
           John Dinh
         </Link>
-        <span id="right-links" className="right-0 absolute top-1/2 transform -translate-y-1/2">
+        <span className="self-center">
           <Link
             to="root"
             onClick={() => scroll.scrollToTop()}
