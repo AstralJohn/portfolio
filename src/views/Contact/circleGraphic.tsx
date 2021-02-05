4c9c31b5d6dd6d9c945e9b4c944ef4764fb6bcd7
John Dinh <johnduydinh@gmail.com>
2021-02-05 12:37:19 -0500

    change container height and width to prevent overlap

--- src/views/Contact/circleGraphic.tsx
+++ src/views/Contact/circleGraphic.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CircleGraphic: React.FC = () => {
   return (
-    <div className="hidden lg:block h-72 w-72 absolute right-0 bottom-0">
+    <div className="hidden lg:block h-1 w-1 absolute right-0 bottom-0">
       <div className="h-52 w-52 bg-blue-circleLight rounded-full absolute -right-20 -bottom-20"></div>
       <div className="h-32 w-32 bg-blue-circleDark rounded-full absolute right-10 bottom-10 opacity-95"></div>
     </div>
